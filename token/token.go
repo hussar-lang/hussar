e97b24001bb0e25ff988299f69ea74049d281df8
kscarlett <kellen@kscarlett.com>
2017-09-26 00:36:19 +0100

    Added tokenisation of identifiers
    - Added numbers and identifiers
    - Added methods to test if a character is numeric or a letter
    - Added keyword lookup

--- token/token.go
+++ token/token.go
@@ -34,3 +34,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
