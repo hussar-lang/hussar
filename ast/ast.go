739f3a90de76f36198b071a40b467c439d9c7493
kscarlett <kellen@kscarlett.com>
2017-12-25 01:31:01 +0100

    Added basics of AST and parser

---
+++ ast/ast.go
@@ -0,0 +1,47 @@
+package ast
+
+import "github.com/kscarlett/kmonkey/token"
+
+type Node interface {
+	TokenLiteral() string // Only needed for testing and debugging
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of the AST
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // = token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // = token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/kscarlett/kmonkey/ast"
+	"github.com/kscarlett/kmonkey/lexer"
+	"github.com/kscarlett/kmonkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
