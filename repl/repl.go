31cb5066c968c915837fbcd6d78cfaf4616cc187
kscarlett <kellen@kscarlett.com>
2017-12-30 21:49:28 +0100

    Putting the 'e' in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/fatih/color"
 
+	"github.com/kscarlett/kmonkey/evaluator"
 	"github.com/kscarlett/kmonkey/lexer"
 	"github.com/kscarlett/kmonkey/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
